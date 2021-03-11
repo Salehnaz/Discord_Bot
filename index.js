4416999b96938f92214c136152f2246137fa9dba
Salehnaz <salehnazimudeen6@gmail.com>
2021-03-11 01:22:59 -0800

    just added comments

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const Discord = require ('discord.js')
 
-require ('dotenv').config();
+require ('dotenv').config();                         // this is for including the .env file to index page
 
-const client = new Discord.Client()
+const client = new Discord.Client()                  // this used for creating a client and we are playing with that client
 
 client.on("ready", () =>{
     console.log('Logged in as  ')
 })
 
-
+                                                   
 client.on("message", msg =>{
     if (msg.content =="ping"){
         msg.reply("pong")
     }
-})
+})                                                   // this function is actually reading msg content and responding . 
 
 
 client.login(process.env.TOKEN)
